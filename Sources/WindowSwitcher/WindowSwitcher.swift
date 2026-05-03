fa3c8e307d8c2128dae25bffd24454c74d68a964
Lorenzo <lorenzoceccotti02@gmail.com>
2026-05-03 18:24:55 +0200

    add handling of SIGTERM

--- Sources/WindowSwitcher/WindowSwitcher.swift
+++ Sources/WindowSwitcher/WindowSwitcher.swift
@@ -7,6 +7,11 @@ import AppKit
 @main
 struct WindowSwitcher {
     static func main() {
+        signal(SIGTERM) { _ in
+            log("WindowSwitcher stopping...")
+            CFRunLoopStop(CFRunLoopGetCurrent())
+        }
+
         let windowManager = WindowManager()
         let focusManager = FocusManager()
         let hotkeyManager = HotkeyManager(windowManager: windowManager, focusManager: focusManager)
