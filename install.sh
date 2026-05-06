759593e51616399fa063783b4e9c80a527135236
Lorenzo <lorenzoceccotti02@gmail.com>
2026-05-06 20:07:51 +0200

    refactor install scripts

--- install.sh
+++ install.sh
@@ -4,19 +4,31 @@ BINARY_PATH="$HOME/bin/WindowSwitcher"
 PLIST_DEST="$HOME/Library/LaunchAgents/local.windowswitcher.plist"
 
 # Clean up any previous installation
-./uninstall.sh
+./uninstall.sh > /dev/null 2>&1
 
 # Build the application
 swift build --configuration release
+if [ $? -ne 0 ]; then
+    echo "Build failed. Please fix the errors and try again."
+		exit 1
+fi
 
 # Install the executable
 mkdir -p "$HOME/bin"
 cp .build/release/WindowSwitcher "$BINARY_PATH"
+if [ $? -ne 0 ]; then
+    echo "Build failed. An error occurred while copying the executable."
+		exit 1
+fi
 
 # Generate the plist with the correct binary path
 sed "s|BINARY_PATH_PLACEHOLDER|$BINARY_PATH|g" ./Resources/local.windowswitcher.plist > "$PLIST_DEST"
 
 # Load the launch agent
 launchctl bootstrap gui/$(id -u) "$PLIST_DEST"
+if [ $? -ne 0 ]; then
+    echo "Failed to load the launch agent. Please check the plist file and try again."
+		exit 1
+fi
 
 echo "Installed successfully. Grant Accessibility permission to $BINARY_PATH in System Settings → Privacy & Security → Accessibility"
--- reload.sh
+++ reload.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Stop the deamon (if it's running)
-launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/local.windowswitcher.plist
+launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/local.windowswitcher.plist > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+		echo "WindowSwitcher daemon stopped successfully."
+fi
 
 # Launch the deamon again
 launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/local.windowswitcher.plist
+if [ $? -eq 0 ]; then
+		echo "WindowSwitcher daemon restarted successfully."
+else
+		echo "Failed to restart the WindowSwitcher daemon. Please check the plist file and try again"
+		exit 1
+fi
